246c2f0180ae1f2721d24d79d83473e3308f5db4
dianamagana20 <ada_hack394@hackademy.mx>
2022-03-23 05:42:27 -0600

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,12 @@
+#Programa que suma los elementos de un arreglo
+
+def sumas():
+    sum=0
+    array=[1,2,3,4,5,6,7,8,9]
+    for n in array:
+        sum=sum + n
+    print (sum)
+
+print("La suma del arreglo es: ")
+suma= sumas()
+
