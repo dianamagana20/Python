08819823b63773b13719993c63c5b115d6bf5c59
dianamagana20 <ada_hack394@hackademy.mx>
2022-03-23 06:05:21 -0600

    Create suma-enteros.py

---
+++ suma-enteros.py
@@ -0,0 +1,12 @@
+
+#Programa de sumar enteros
+
+def sumar(x,y):
+    print("La suma es:")
+    sum = x + y
+    print(sum)
+
+x=int(input("Ingrese el primer numero entero: "))
+y=int(input("Ingrese el segundo numero entero: "))
+suma=sumar(x,y)
+
